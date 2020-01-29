91b67042aae9bf4afb0174a102b6e64c3f1920e2
Keyur <keyur@moesif.com>
2020-01-28 17:42:30 -0800

    Add: Utf8-Validation and decompress the body if not valid body
    Add: Utf8-Validation and decompress the body if not valid body
    Refactor: Safely parse json body using cjson.safe
    Bump version to 0.2.6

--- kong/plugins/moesif/handler.lua
+++ kong/plugins/moesif/handler.lua
@@ -93,7 +93,7 @@ function MoesifLogHandler:init_worker()
 end
 
 MoesifLogHandler.PRIORITY = 5
-MoesifLogHandler.VERSION = "0.2.5"
+MoesifLogHandler.VERSION = "0.2.6"
 
 -- Plugin version
 plugin_version = MoesifLogHandler.VERSION
--- kong/plugins/moesif/moesif_ser.lua
+++ kong/plugins/moesif/moesif_ser.lua
@@ -4,12 +4,14 @@ local req_start_time = ngx.req.start_time
 local req_get_headers = ngx.req.get_headers
 local res_get_headers = ngx.resp.get_headers
 local cjson = require "cjson"
+local cjson_safe = require "cjson.safe"
 local _M = {}
 local ngx_log = ngx.log
 local ngx_log_ERR = ngx.ERR
 local client_ip = require "kong.plugins.moesif.client_ip"
 local zzlib = require "kong.plugins.moesif.zzlib"
 local base64 = require "kong.plugins.moesif.base64"
+local utf8_validator = require "kong.plugins.moesif.utf8_validator"
 
 function mask_body(body, masks)
   if masks == nil then return body end
@@ -27,11 +29,20 @@ function mask_body(body, masks)
   return body
 end
 
+function base64_encode_body(body)
+  return base64.encode(body), 'base64'
+end
+
 function transform_body(body)
   if type(body) == "string" and string.sub(body, 1, 1) == "{" or string.sub(body, 1, 1) == "[" then
-    return cjson.decode(body), 'json'
+    local decoded_body = cjson_safe.decode(body)
+    if not decoded_body then 
+      return base64_encode_body(body)
+    else
+      return decoded_body, 'json' 
+    end
   else
-    return base64.encode(body), 'base64'
+    return base64_encode_body(body)
   end
 end
 
@@ -42,17 +53,38 @@ function process_data(body, mask_fields)
   if next(mask_fields) == nil then
     body_entity, body_transfer_encoding = transform_body(body)
   else
-    local is_decoded, decoded_body = pcall(cjson.decode, body)
+    local is_decoded, decoded_body = pcall(cjson_safe.decode, body)
     if not is_decoded then 
       body_entity, body_transfer_encoding = transform_body(body)
-    else
+    elseif (decoded_body ~= nil) then
       local ok, mask_result = pcall(mask_body, decoded_body, mask_fields)
       if not ok then
         body_entity, body_transfer_encoding = transform_body(body)
       else
         body_entity, body_transfer_encoding = transform_body(cjson.encode(mask_result))
       end
+    else
+      body_entity, body_transfer_encoding = transform_body(body)
     end 
+  end
+  return body_entity, body_transfer_encoding
+end
+
+function decompress_body(body, masks)
+  local body_entity = nil
+  local body_transfer_encoding = nil
+
+  local ok, decompressed_body = pcall(zzlib.gunzip, body)
+  if not ok then
+    if debug then
+      ngx_log(ngx_log_ERR, "[moesif] failed to decompress body: ", decompressed_body)
+    end
+    body_entity, body_transfer_encoding = base64_encode_body(body)
+  else
+    if debug then
+      ngx_log(ngx.DEBUG, " [moesif]  ", "successfully decompressed body: ")
+    end
+    body_entity, body_transfer_encoding = process_data(decompressed_body, masks)
   end
   return body_entity, body_transfer_encoding
 end
@@ -66,6 +98,9 @@ function _M.serialize(ngx, conf)
   local req_body_transfer_encoding = nil
   local rsp_body_transfer_encoding = nil
 
+  local request_headers
+  request_headers = req_get_headers()
+
   local response_headers
   response_headers = res_get_headers()
 
@@ -77,13 +112,23 @@ function _M.serialize(ngx, conf)
   if moesif_ctx.req_body == nil or conf.disable_capture_request_body then
     request_body_entity = nil
   else
-    request_body_entity, req_body_transfer_encoding = process_data(moesif_ctx.req_body, conf.request_masks)
+    local is_valid_request_body = utf8_validator.validate(moesif_ctx.req_body)
+    if not is_valid_request_body then
+      request_body_entity, req_body_transfer_encoding = decompress_body(moesif_ctx.req_body, conf.request_masks)
+    else
+      request_body_entity, req_body_transfer_encoding = process_data(moesif_ctx.req_body, conf.request_masks)
+    end 
   end
 
   if moesif_ctx.res_body == nil or conf.disable_capture_response_body then
     response_body_entity = nil
   else
-    response_body_entity, rsp_body_transfer_encoding = process_data(moesif_ctx.res_body, conf.response_masks)
+    local is_valid_response_body = utf8_validator.validate(moesif_ctx.res_body)
+    if not is_valid_response_body then
+      response_body_entity, rsp_body_transfer_encoding = decompress_body(moesif_ctx.res_body, conf.response_masks)
+    else
+      response_body_entity, rsp_body_transfer_encoding = process_data(moesif_ctx.res_body, conf.response_masks)
+    end
   end
 
   if ngx.ctx.authenticated_credential ~= nil then
@@ -98,37 +143,23 @@ function _M.serialize(ngx, conf)
     session_token_entity = nil
   end
 
-  if req_get_headers()["x-consumer-custom-id"] ~= nil then
-    user_id_entity = tostring(req_get_headers()["x-consumer-custom-id"])
-  elseif req_get_headers()["x-consumer-username"] ~= nil then
-    user_id_entity = tostring(req_get_headers()["x-consumer-username"])
-  elseif req_get_headers()["x-consumer-id"] ~= nil then
-    user_id_entity = tostring(req_get_headers()["x-consumer-id"])
+  if request_headers["x-consumer-custom-id"] ~= nil then
+    user_id_entity = tostring(request_headers["x-consumer-custom-id"])
+  elseif request_headers["x-consumer-username"] ~= nil then
+    user_id_entity = tostring(request_headers["x-consumer-username"])
+  elseif request_headers["x-consumer-id"] ~= nil then
+    user_id_entity = tostring(request_headers["x-consumer-id"])
   else
     user_id_entity = nil
   end
 
-  if (response_headers["content-encoding"] ~= nil) and (response_headers["content-encoding"] == 'gzip') then 
-    local ok, decompressed_body = pcall(zzlib.gunzip, response_body_entity)
-      if not ok then
-        if debug then
-          ngx_log(ngx_log_ERR, "[moesif] failed to decompress body: ", decompressed_body)
-        end
-      else
-        if debug then
-          ngx_log(ngx.DEBUG, " [moesif]  ", "successfully decompressed body: ")
-        end
-        response_body_entity = decompressed_body
-      end
-  end
-
-   return {
+  return {
     request = {
       uri =  ngx.var.scheme .. "://" .. ngx.var.host .. ":" .. ngx.var.server_port .. ngx.var.request_uri,
-      headers = req_get_headers(),
+      headers = request_headers,
       body = request_body_entity,
       verb = req_get_method(),
-      ip_address = client_ip.get_client_ip(req_get_headers()),
+      ip_address = client_ip.get_client_ip(request_headers),
       api_version = ngx.ctx.api_version,
       time = os.date("!%Y-%m-%dT%H:%M:%S.", req_start_time()) .. string.format("%d",(req_start_time()- string.format("%d", req_start_time()))*1000),
       transfer_encoding = req_body_transfer_encoding,
@@ -144,7 +175,7 @@ function _M.serialize(ngx, conf)
     session_token = session_token_entity,
     user_id = user_id_entity,
     direction = "Incoming"
-}
+  }
 end
 
 return _M
