1721618d437f39d7a919312df5c6b4c216e3d583
Keyur <keyur.kv@gmail.com>
2020-03-31 15:54:38 -0400

    Add: Support for Mask Header fields
    Add: Support for Mask Header fields
    Refactor: request_masks -> request_body_masks configuration
    Refactor: Backward compatibility for request_masks configuration
    Refactor: Update README.md
    Bump version to 0.2.8

--- kong/plugins/moesif/handler.lua
+++ kong/plugins/moesif/handler.lua
@@ -93,7 +93,7 @@ function MoesifLogHandler:init_worker()
 end
 
 MoesifLogHandler.PRIORITY = 5
-MoesifLogHandler.VERSION = "0.2.7"
+MoesifLogHandler.VERSION = "0.2.8"
 
 -- Plugin version
 plugin_version = MoesifLogHandler.VERSION
