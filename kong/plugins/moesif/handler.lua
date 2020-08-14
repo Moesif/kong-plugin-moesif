3b3bedb887618756f96884c9a3f6fc92f24aa091
Keyur <keyur.kv@gmail.com>
2020-08-13 20:55:43 -0400

    Fix: Use local variable host and port
    Fix: Use local variable host and port
    Fix: Add checks to see if sock object is not nil
    Bump version to 0.2.13

--- kong/plugins/moesif/handler.lua
+++ kong/plugins/moesif/handler.lua
@@ -93,7 +93,7 @@ function MoesifLogHandler:init_worker()
 end
 
 MoesifLogHandler.PRIORITY = 5
-MoesifLogHandler.VERSION = "0.2.12"
+MoesifLogHandler.VERSION = "0.2.13"
 
 -- Plugin version
 plugin_version = MoesifLogHandler.VERSION
