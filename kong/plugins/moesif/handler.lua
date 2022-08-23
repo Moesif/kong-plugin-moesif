e46f133305faf2c6f96197d1ee6464c912eeb973
xiaokanghuo <sean@moesif.com>
2022-08-23 14:06:56 -0700

    Add Graphql regex in Governance rules
    1. bumped up version to 1.0.15
    2. update rockspec version and file name

--- kong/plugins/moesif/handler.lua
+++ kong/plugins/moesif/handler.lua
@@ -164,7 +164,7 @@ function MoesifLogHandler:init_worker()
 end
 
 MoesifLogHandler.PRIORITY = 5
-MoesifLogHandler.VERSION = "1.0.14"
+MoesifLogHandler.VERSION = "1.0.15"
 
 -- Plugin version
 plugin_version = MoesifLogHandler.VERSION
