08fbcf8298413eb9725746d7d7d92702426be1e7
Keyur <keyur.kv@gmail.com>
2023-10-25 18:26:58 -0700

    #186282816 Refactor: Ensure entity_rules_hashes doesn't reset often (#97)
    Refactor: Ensure entity_rules_hashes doesn't reset often
    Fix: Regex to fetch configETag and rulesTag correctly
    Bump version to 2.0.10

--- kong/plugins/moesif/handler.lua
+++ kong/plugins/moesif/handler.lua
@@ -1,7 +1,7 @@
 local serializer = require "kong.plugins.moesif.moesif_ser"
 local governance = require "kong.plugins.moesif.moesif_gov"
 local MoesifLogHandler = {
-  VERSION  = "2.0.9",
+  VERSION  = "2.0.10",
   PRIORITY = 5,
 }
 local log = require "kong.plugins.moesif.log"
