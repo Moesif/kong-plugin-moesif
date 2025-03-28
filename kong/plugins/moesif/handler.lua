a73ce5ec9d77a0449709e9829eb7f195c813aae9
Keyur <keyur.kv@gmail.com>
2025-03-28 16:25:11 -0700

    Refactor: Comment out access phase
    Refactor: Comment out access phase

--- kong/plugins/moesif/handler.lua
+++ kong/plugins/moesif/handler.lua
@@ -4,7 +4,7 @@ local MoesifLogHandler = {
   VERSION  = "2.1.1",
   PRIORITY = 5,
 }
-local log = require "kong.plugins.moesif.log"
+-- local log = require "kong.plugins.moesif.log"
 local req_set_header = ngx.req.set_header
 local string_find = string.find
 local req_read_body = ngx.req.read_body
@@ -23,71 +23,71 @@ local function uuid()
     end)	
 end
 
-function MoesifLogHandler:access(conf)
+-- function MoesifLogHandler:access(conf)
 
-  local start_access_phase_time = socket.gettime()*1000
+--   local start_access_phase_time = socket.gettime()*1000
 
-  local headers = req_get_headers()
-  -- Add Transaction Id to the request header	
-  if not conf.disable_transaction_id then	
-    if headers["X-Moesif-Transaction-Id"] ~= nil then	
-      local req_trans_id = headers["X-Moesif-Transaction-Id"]	
-      if req_trans_id ~= nil and req_trans_id:gsub("%s+", "") ~= "" then	
-        ngx.ctx.transaction_id = req_trans_id	
-      else	
-        ngx.ctx.transaction_id = uuid()	
-      end	
-    else	
-      ngx.ctx.transaction_id = uuid()	
-    end	
-  -- Add Transaction Id to the request header	
-  req_set_header("X-Moesif-Transaction-Id", ngx.ctx.transaction_id)	
-  end
+--   local headers = req_get_headers()
+--   -- Add Transaction Id to the request header	
+--   if not conf.disable_transaction_id then	
+--     if headers["X-Moesif-Transaction-Id"] ~= nil then	
+--       local req_trans_id = headers["X-Moesif-Transaction-Id"]	
+--       if req_trans_id ~= nil and req_trans_id:gsub("%s+", "") ~= "" then	
+--         ngx.ctx.transaction_id = req_trans_id	
+--       else	
+--         ngx.ctx.transaction_id = uuid()	
+--       end	
+--     else	
+--       ngx.ctx.transaction_id = uuid()	
+--     end	
+--   -- Add Transaction Id to the request header	
+--   req_set_header("X-Moesif-Transaction-Id", ngx.ctx.transaction_id)	
+--   end
 
 
-  local req_body, res_body = "", ""
-  local req_post_args = {}
-  local err = nil
-  local mimetype = nil
-  -- Only capture the request body if conf.disable_capture_request_body is false
-  if not conf.disable_capture_request_body then
-    local content_length = headers["content-length"]
-    -- Hash key of the config application Id
-    local hash_key = string.sub(conf.application_id, -10)
-    if (queue_hashes[hash_key] == nil) or 
-          (queue_hashes[hash_key] ~= nil and type(queue_hashes[hash_key]) == "table" and #queue_hashes[hash_key] < conf.event_queue_size) then
+--   local req_body, res_body = "", ""
+--   local req_post_args = {}
+--   local err = nil
+--   local mimetype = nil
+--   -- Only capture the request body if conf.disable_capture_request_body is false
+--   if not conf.disable_capture_request_body then
+--     local content_length = headers["content-length"]
+--     -- Hash key of the config application Id
+--     local hash_key = string.sub(conf.application_id, -10)
+--     if (queue_hashes[hash_key] == nil) or 
+--           (queue_hashes[hash_key] ~= nil and type(queue_hashes[hash_key]) == "table" and #queue_hashes[hash_key] < conf.event_queue_size) then
 
-      -- Read request body
-      req_read_body()
-      local read_request_body = req_get_body_data()
-      if (content_length == nil and read_request_body ~= nil and string.len(read_request_body) <= conf.request_max_body_size_limit) or (content_length ~= nil and tonumber(content_length) <= conf.request_max_body_size_limit) then 
-        req_body = read_request_body
-        local content_type = headers["content-type"]
-        if content_type and string_find(content_type:lower(), "application/x-www-form-urlencoded", nil, true) then
-          req_post_args, err, mimetype = kong.request.get_body()
-        end
-      end
-    end
-  end
-    ngx.ctx.api_version = conf.api_version
--- keep in memory the bodies for this request
-  ngx.ctx.moesif = {
-    req_body = req_body,
-    res_body = res_body,
-    req_post_args = req_post_args,
-    res_body_exceeded_max_size = false
-  }
+--       -- Read request body
+--       req_read_body()
+--       local read_request_body = req_get_body_data()
+--       if (content_length == nil and read_request_body ~= nil and string.len(read_request_body) <= conf.request_max_body_size_limit) or (content_length ~= nil and tonumber(content_length) <= conf.request_max_body_size_limit) then 
+--         req_body = read_request_body
+--         local content_type = headers["content-type"]
+--         if content_type and string_find(content_type:lower(), "application/x-www-form-urlencoded", nil, true) then
+--           req_post_args, err, mimetype = kong.request.get_body()
+--         end
+--       end
+--     end
+--   end
+--     ngx.ctx.api_version = conf.api_version
+-- -- keep in memory the bodies for this request
+--   ngx.ctx.moesif = {
+--     req_body = req_body,
+--     res_body = res_body,
+--     req_post_args = req_post_args,
+--     res_body_exceeded_max_size = false
+--   }
 
-  -- -- Check if need to block incoming request based on user-specified governance rules
-  -- local block_req = governance.govern_request(ngx, conf, start_access_phase_time)
-  -- if block_req == nil then 
-  --   if conf.debug then
-  --     ngx.log(ngx.DEBUG, '[moesif] No need to block incoming request.')
-  --   end
-  --   local end_access_phase_time = socket.gettime()*1000
-  --   ngx.log(ngx.DEBUG, "[moesif] access phase took time for non-blocking request - ".. tostring(end_access_phase_time - start_access_phase_time).." for pid - ".. ngx.worker.pid())
-  -- end
-end
+--   -- -- Check if need to block incoming request based on user-specified governance rules
+--   -- local block_req = governance.govern_request(ngx, conf, start_access_phase_time)
+--   -- if block_req == nil then 
+--   --   if conf.debug then
+--   --     ngx.log(ngx.DEBUG, '[moesif] No need to block incoming request.')
+--   --   end
+--   --   local end_access_phase_time = socket.gettime()*1000
+--   --   ngx.log(ngx.DEBUG, "[moesif] access phase took time for non-blocking request - ".. tostring(end_access_phase_time - start_access_phase_time).." for pid - ".. ngx.worker.pid())
+--   -- end
+-- end
 
 -- function MoesifLogHandler:body_filter(conf)
 --   -- Only capture the response body if conf.disable_capture_response_body is false
